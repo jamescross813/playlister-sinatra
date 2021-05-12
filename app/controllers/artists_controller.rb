4fc3fa2b91cef449f59538ae4a599c115a365c95
jamescross <crossjames813@gmail.com>
2021-05-11 21:36:50 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,8 +9,7 @@ class ArtistsController < Sinatra::Base
     end
   
     get '/artists/:slug' do
-      slug = params[:slug]
-      @artist = Artist.find_by_slug(slug)
+      @artist = Artist.find {|artist| artist.slug == params[:slug]}
       erb :"artists/show"
     end
   end
--- app/models/songgenre.rb
+++ app/models/songgenre.rb
@@ -1,4 +1,6 @@
+require_relative './concerns/slugifiable.rb'
 class SongGenre < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
     belongs_to :song
     belongs_to :genre
